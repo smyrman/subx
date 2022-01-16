9142703bb7408105cedb440f1f3d7703f7e0aa34
Sindre Myren <sindre@clarify.io>
2022-01-16 19:35:58 +0100

    add new type Accumulator for combining errors
    Accumulator allows accumulating error with less boiler plate.

--- error.go
+++ error.go
@@ -1,11 +1,11 @@
 package subx
 
 import (
-	"fmt"
 	"bytes"
+	"fmt"
 )
 
-type cmpError struct{
+type cmpError struct {
 	suffix string
 	result interface{}
 	expect []interface{}
@@ -55,4 +55,47 @@ func (errs Errors) Error() string {
 		}
 	}
 	return buf.String()
+}
+
+
+
+// Accumulator allows accumulating errors.
+type Accumulator struct{
+	prefix string
+	errs Errors
+}
+
+// SetPrefix to use on all errors.
+func(acc *Accumulator) SetPrefix(prefix string) {
+	acc.prefix = prefix
+}
+
+// Register adds an error to acc if it's not nil.
+func (acc *Accumulator) Register(err error) {
+	if err != nil {
+		acc.errs = append(acc.errs, err)
+	}
+}
+
+// Registerf adds an error to acc if it's not nil using the passed in formatting
+// string.
+func (acc *Accumulator) Registerf(format string, err error) {
+	if err != nil {
+		acc.errs = append(acc.errs, fmt.Errorf(format, err))
+	}
+}
+
+// Result returns the accumulated error or nil.
+func (acc *Accumulator) Result() error {
+	switch len(acc.errs) {
+	case 0:
+		return nil
+	case 1:
+		if acc.prefix == "" {
+			return acc.errs[0]
+		}
+		return fmt.Errorf("%s%w", acc.prefix, acc.errs[0])
+	default:
+		return fmt.Errorf("%s\n%w", acc.prefix, acc.errs)
+	}
 }
